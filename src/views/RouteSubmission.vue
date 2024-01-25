5d4f6a341fc68d837b26236c3c651a0115be5527
Gemma Moore <GemmaMoore2021@gmail.com>
2024-01-25 11:57:07 -0600

    handle file method

--- src/views/RouteSubmission.vue
+++ src/views/RouteSubmission.vue
@@ -10,11 +10,14 @@
         <br>
     </v-container>
 
-    <v-container class="formContainer">
+    <form class="formContainer">
 
         <v-text-field 
             label="Route Name"
-            prepend-icon="mdi-bike">
+            prepend-icon="mdi-bike"
+            v-model="name"
+            name="name"
+            >
 
         </v-text-field> 
     
@@ -23,20 +26,59 @@
           variant="outlined"
           type="file"
           ref="file"
-          @change="handleFile(file)"
+          @change="handleFile()"
           required
+          v-model="file"
+          name="file"
         ></v-file-input>
 
         <v-btn class="submit" type="submit">submit</v-btn>
 
-    </v-container>
+    </form>
 
 </v-container>
 
 
 </template>
 
-<script import emailjs from 'emailjs-com'> 
+<script> 
+import {emailjs} from '@emailjs/browser';
+
+// get form data 
+export default {
+  name: 'routeSuggestion',
+  data() {
+    return {
+      name: '',
+      file: '',
+     
+    }
+  },
+  methods: {
+    sendEmail(e) {
+      try {
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target,
+        'YOUR_USER_ID', {
+          name: this.name,
+          file: this.file
+          
+        })
+
+      } catch(error) {
+          console.log({error})
+      }
+      // Reset form field
+      this.name = ''
+      this.file = ''
+      
+    },
+
+    handlefile()
+    { 
+      const selectedFile = this.$refs.file.files[0];
+    }
+  }
+}
 
 
 
