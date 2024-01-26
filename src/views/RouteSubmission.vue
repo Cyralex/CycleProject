2981ebf3a8aad1fc1493627cb9b6fe1ddf56a197
JePhip <jesaiahphipps@gmail.com>
2024-01-26 17:22:54 -0600

    emailing limited to 50kb - emailjs

--- src/views/RouteSubmission.vue
+++ src/views/RouteSubmission.vue
@@ -1,103 +1,93 @@
-<template> 
-
-<v-container>
-    <v-container class="description"> 
-        Please use the form below if you have a route you would like to see included in the Bolivar Cycling website!
-        <br>
-        Please provide a name for the route along with its associated GPX file. 
-        <br>
-        Your submission will be reviewed by the City of Bolivar.
-        <br>
+<template>
+  <v-container>
+    <v-container class="description">
+      Please use the form below if you have a route you would like to see
+      included in the Bolivar Cycling website!
+      <br />
+      Please provide a name for the route along with its associated GPX file.
+      <br />
+      Your submission will be reviewed by the City of Bolivar.
+      <br />
     </v-container>
 
-    <form class="formContainer" 
-          @submit.prevent="sendEmail" 
-          enctype="multipart/form-data"
-          id="form"
-          name="form">
+    <form
+      class="formContainer"
+      @submit.prevent="sendEmail"
+      enctype="multipart/form-data"
+      id="form"
+      name="form"
+      ref="form"
+    >
+      <v-text-field
+        label="Route Name"
+        prepend-icon="mdi-bike"
+        v-model="name"
+        name="name"
+        id="name"
+        required
+      >
+      </v-text-field>
 
-        <v-text-field 
-            label="Route Name"
-            prepend-icon="mdi-bike"
-            v-model="name"
-            name="name"
-            id="name"
-            required
-            >
+      <v-file-input
+        label="Upload .gpx File"
+        variant="outlined"
+        type="file"
+        ref="file"
+        @change="handleFile()"
+        required
+        name="file"
+        accept=".gpx"
+      ></v-file-input>
 
-        </v-text-field> 
-    
-        <v-file-input
-          label="Upload .gpx File"
-          variant="outlined"
-          type="file"
-          ref="file"
-          @change="handleFile()"
-          required         
-          name="file"
-          accept=".gpx"
-        ></v-file-input>
-
-        <v-btn class="submit" type="submit">submit</v-btn>
-
+      <v-btn class="submit" type="submit">submit</v-btn>
     </form>
-
-</v-container>
-
-
+  </v-container>
 </template>
 
-<script> 
-// import {emailjs} from '@emailjs/browser';
+<script>
+import emailjs from "@emailjs/browser";
 
- 
 export default {
-  name: 'routeSuggestion',
+  name: "routeSuggestion",
   data() {
     return {
-      name: '',
-      file: null
-     
+      name: "",
+      file: null,
     };
-  },  
+  },
 
   methods: {
-    
-    handleFile(){
-   const selectedFile = this.$refs.file.files[0];
-  },     
-    
-      sendEmail() {
-      
-      try
-      { 
-
-        emailjs.sendForm('service_ind2c06', 'template_z4rluov', 'jiDg78lBJ8doaLmoh' )
-      }
-
-      catch(err)
-      { 
-        console.log("error sending email");
-      }
-      
-    }   
-
-  }
-}
-
+    handleFile() {
+      const selectedFile = this.$refs.file.files[0];
+    },
+    sendEmail() {
+      emailjs
+        .sendForm(
+          "service_ind2c06",
+          "template_z4rluov",
+          this.$refs.form,
+          "jiDg78lBJ8doaLmoh"
+        )
+        .then(
+          (result) => {
+            console.log("SUCCESS!", result.text);
+          },
+          (error) => {
+            console.log("FAILED...", error.text);
+          }
+        );
+    },
+  },
+};
 </script>
 
-<style> 
-    .formContainer
-    { 
-        max-width: 500px;
-        color: #083a8c;
-    }
+<style>
+.formContainer {
+  max-width: 500px;
+  color: #083a8c;
+}
 
-    .description
-    { 
-        text-align: center;       
-    }
-
-   
+.description {
+  text-align: center;
+}
 </style>
