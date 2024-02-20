4d05c30fd1bc98629bd968bfb260ee0206586949
Gemma Moore <GemmaMoore2021@gmail.com>
2024-02-20 09:49:04 -0600

    configured vitest

---
+++ vite.config.mjs
@@ -0,0 +1,8 @@
+import vue from '@vitejs/plugin-vue';
+import jsconfigPaths from 'vite-jsconfig-paths';
+import { defineConfig } from "vite";
+
+
+export default{
+    plugins: [vue(), jsconfigPaths()],
+}
