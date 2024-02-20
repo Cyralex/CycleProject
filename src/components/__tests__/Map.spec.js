1276f96e43102a35e8a4ca11d84449b35bf1b343
Gemma Moore <GemmaMoore2021@gmail.com>
2024-02-20 09:56:10 -0600

    unit test for footer rendering passed

--- src/components/__tests__/Map.spec.js
+++ src/components/__tests__/Map.spec.js
@@ -1,11 +1,11 @@
-import { describe, it, expect, } from 'vitest'
-import {shallowMount } from '@vue/test-utils'
-import Map from '../Map.vue'
+import { describe, it, expect} from 'vitest';
+import {shallowMount } from '@vue/test-utils';
+import Map from '../Map.vue';
 
 
 describe('Map.vue Test', () => {
     it('renders without errors', () => {
         const wrapper = shallowMount(Map);
-        expect(wrapper.exists()).toBeTruthy()
+        expect(wrapper.exists()).toBeTruthy();
     })
 })
