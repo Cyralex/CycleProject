c8f685e327086c3e14522f13469c419eae543209
Gemma Moore <GemmaMoore2021@gmail.com>
2024-02-23 22:24:59 -0600

    removed problematic test to restart

--- src/components/__tests__/Map.spec.js
+++ src/components/__tests__/Map.spec.js
@@ -1,11 +1,13 @@
 import { describe, it, expect} from 'vitest';
-import {shallowMount } from '@vue/test-utils';
+import {mount} from '@vue/test-utils';
 import Map from '../Map.vue';
 
 
 describe('Map.vue Test', () => {
-    it('renders without errors', () => {
-        const wrapper = shallowMount(Map);
+    it('mounts and renders correctly', () => {
+        const wrapper = mount(Map);
         expect(wrapper.exists()).toBeTruthy();
-    })
-})
+        expect(wrapper.find('.mapContainer')).toBeTruthy();
+        expect(wrapper.findComponent(LMap)).toBeTruthy();
+    });
+});
