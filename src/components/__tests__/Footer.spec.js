923124c99cce695f043b90f6c5c866fed6697fb5
Gemma Moore <GemmaMoore2021@gmail.com>
2024-02-26 10:20:53 -0600

    test footer for number and content of links

--- src/components/__tests__/Footer.spec.js
+++ src/components/__tests__/Footer.spec.js
@@ -1,10 +1,57 @@
-import {describe, it, expect} from 'vitest';
-import {shallowMount } from '@vue/test-utils';
+import {describe, it, expect, vi} from 'vitest';
+import {shallowMount} from '@vue/test-utils';
+import {mount} from '@vue/test-utils';
 import Footer from '../nav/Footer.vue'
 
+
 describe('Footer.vue test', () => {
+
+    // test if componenet renders
     it('renders without errors', () => {
         const wrapper = shallowMount(Footer);
         expect(wrapper.exists()).toBeTruthy();
-    })
-})
+    });
+
+    // check for correct number of links 
+    it('renders links correctly', () => {
+        const wrapper = mount(Footer);
+        const links = wrapper.findAll('v-btn');
+        expect(links.length).toBe(3);   
+    });
+
+    // check for correct links 
+    it('links array contains expected "to" values', () => {
+        const wrapper = mount(Footer);
+    
+        // Access the links data
+        const links = wrapper.vm.links;
+    
+        // Assert the "to" property values for each link
+        links.forEach((link, index) => {
+        expect(link.to).toBe([
+            '/', 
+            '/about', 
+            'https://bolivar.mo.us/contact-us/',
+        ][index]);
+        });
+    });
+
+    // check for correct name of link 
+    it('links array contains expected "name" values', () => {
+        const wrapper = mount(Footer);
+    
+        // Access the links data
+        const links = wrapper.vm.links;
+    
+        // Assert the "to" property values for each link
+        links.forEach((link, index) => {
+        expect(link.name).toBe([
+            'Home', 
+            'About', 
+            'Contact Us',
+        ][index]);
+        });
+    });   
+
+   
+});
