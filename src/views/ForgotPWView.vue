3e08369f1511d23f2bc99fa275ac2c640aa270be
JePhip <jesaiahphipps@gmail.com>
2024-04-24 09:27:09 -0500

    functioning

--- src/views/ForgotPWView.vue
+++ src/views/ForgotPWView.vue
@@ -1,54 +1,80 @@
 <template>
   <v-row justify="center">
-    <h1>you have a bad memory</h1>
-  </v-row>
-  <v-row justify="center">
-    <v-form @submit.prevent="submitForm()" ref="form">
-      <div class="text-h4">Enter the email associated with your account</div>
-      <v-text-field
-        v-model="email"
+    <v-col cols="12" sm="8" md="6" lg="4" class="d-flex align-center">
+      <v-form
+        class="routeForm"
+        validate-on="submit lazy"
         :rules="emailRules"
-        label="name@example.com"
-        hide-details
-        required
-      ></v-text-field>
-      <v-btn type="submit">Submit</v-btn>
-    </v-form>
+        @submit.prevent="submitForm()"
+        ref="form"
+      >
+        <div class="text-h5 text-center mb-6">
+          Enter the email associated with your account
+        </div>
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="name@example.com"
+          hide-details
+          required
+          class="mb-4"
+        ></v-text-field>
+        <v-row justify="center">
+          <v-col>
+            <v-btn type="submit" class="mt-4">Submit</v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
+    </v-col>
+  </v-row>
+
+  <v-row justify="center">
+    <div style="padding-top: 50px" v-if="emailValidity">
+      <v-alert
+        class="success"
+        type="success"
+        title="New password sent!"
+        text="A new password has been sent to the admin email"
+      >
+      </v-alert>
+    </div>
   </v-row>
 </template>
 
 <script setup>
 import { ref, reactive } from "vue";
+import { useRouter } from "vue-router";
+const router = useRouter();
+
 const email = ref("");
 let form = reactive();
 const emailRules = reactive([
   (value) => /.+@.+\..+/.test(value) || "E-mail must be valid.",
 ]);
-
+let emailValidity = ref(false);
 const submitForm = async () => {
-
-    //ask backend if email is correct
-    let reqObj = {
-      email: email.value,
-    };
-    let response = await fetch(
-      `${process.env.VUE_APP_BASE_URL}/v1/email/validate`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(reqObj),
-      }
-    );
-    response = await response.json();
-    console.log("validate res from be", response);
-    if (response.valid == true) {
-      console.log("email correct");
-    } else {
-      console.log("incorrect");
+  //ask backend if email is correct
+  let reqObj = {
+    email: email.value,
+  };
+  let response = await fetch(
+    `${process.env.VUE_APP_BASE_URL}/v1/email/validate`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reqObj),
     }
-
+  );
+  response = await response.json();
+  if (response.valid == true) {
+    emailValidity.value = true;
+    setTimeout(function () {
+      emailValidity.value = false;
+      router.push("/login");
+    }, 10000);
+  }
 };
 </script>
 
